0f1adc49bad532b04a872f084ad06a06e3aa6195
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-11-03 14:28:06 -0300

    refactor: remove unused import

--- src/modules/store-catalog/facade/store-catalog.facade.ts
+++ src/modules/store-catalog/facade/store-catalog.facade.ts
@@ -1,4 +1,3 @@
-import Product from '../domain/product.entity'
 import FindAllProductsUseCase from '../usecase/find-all-products/find-all-products.usecase'
 import FindProductUseCase from '../usecase/find-product/find-product.usecase'
 import StoreCatalogFacadeInterface, {
